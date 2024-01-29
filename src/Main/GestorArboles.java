7bff2d9fc6c9c204769aefafe332a9b823f78e93
Xabier Vacas <ikcty@plaiaundi.net>
2024-01-29 13:04:48 +0100

    Conexion a la base de datos

--- src/Main/GestorArboles.java
+++ src/Main/GestorArboles.java
@@ -1,10 +1,42 @@
 package Main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class GestorArboles {
 
+	private static String HOST="localhost";
+	private static String BBDD="eh_garden";
+	private static String USER="root";
+	private static String PASSWORD="";
+	private static Scanner scan = new Scanner (System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		run();
 	}
+
+	private static void run() {
+		
+			try {
+				//Cargar libreria
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				
+				//Crear conexion
+				Connection conexion = DriverManager.getConnection("jdbc:mysql://"+HOST+"/"+BBDD,USER,PASSWORD);
+				
+			} catch (ClassNotFoundException e) {
+				System.out.println("Error de importacion de la libreria");
+				e.printStackTrace();
+			}
+		
+			catch (SQLException e) {
+				System.out.println("Error al conectarse a la base de datos");
+				e.printStackTrace();
+			}
+			
+	}
+	
 
 }
