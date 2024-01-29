1ed7ef1d1767f2745d6f5b7cae18cbdcfc329b1d
Xabier Vacas <ikcty@plaiaundi.net>
2024-01-29 13:35:25 +0100

    Implementacion metodo menu

--- src/Main/GestorArboles.java
+++ src/Main/GestorArboles.java
@@ -10,11 +10,17 @@ import java.util.Scanner;
 
 public class GestorArboles {
 
-	private static String HOST="localhost";
-	private static String BBDD="eh_garden";
-	private static String USER="root";
-	private static String PASSWORD="";
-	private static Scanner scan = new Scanner (System.in);
+	private static final String HOST="localhost";
+	private static final String BBDD="eh_garden";
+	private static final String USER="root";
+	private static final String PASSWORD="";
+	private static final Scanner scan = new Scanner (System.in);
+	
+	private static final int SALIR = 0;
+	private static final int CREATE = 1;
+	private static final int READ = 2;
+	private static final int UPDATE = 3;
+	private static final int DELETE = 4;
 	
 	public static void main(String[] args) {
 		run();
@@ -25,15 +31,21 @@ public class GestorArboles {
 		
 			try {
 				//Cargar libreria
-				Class.forName("com.mysql.cj.jdbc.Driver");
+					Class.forName("com.mysql.cj.jdbc.Driver");
 				
 				//Crear conexion
-				Connection conexion = DriverManager.getConnection("jdbc:mysql://"+HOST+"/"+BBDD,USER,PASSWORD);
+					Connection conexion = DriverManager.getConnection("jdbc:mysql://"+HOST+"/"+BBDD,USER,PASSWORD);
 				
 				//Crear Statement
-				Statement st = conexion.createStatement();
+					Statement st = conexion.createStatement();
 				
-				arboles(st);
+				
+				//Variables
+					ArrayList<Arbol> arboles = new ArrayList<>();
+				
+				//Menu
+					menu();
+					
 			} catch (ClassNotFoundException e) {
 				System.out.println("Error de importacion de la libreria");
 				e.printStackTrace();
@@ -46,33 +58,36 @@ public class GestorArboles {
 			
 	}
 	
-	private static ArrayList<Arbol> arboles(Statement st){
-		ArrayList<Arbol> arboles = new ArrayList<>();
+	private static void arboles(Statement st,	ArrayList<Arbol> arboles){
 		
 		try {
 			//Ejecutar Query
-			ResultSet rs = st.executeQuery("SELECT * FROM arboles");
+				ResultSet rs = st.executeQuery("SELECT * FROM arboles");
 			
-			//Recorrer
-			while(rs.next()){
-				Arbol a = new Arbol();
-				a.setId(rs.getInt("id"));
-				a.setNombreComun(rs.getString("nombre_comun"));
-				a.setNombreCientifico(rs.getString("nombre_cientifico"));
-				a.setHabitat(rs.getString("habitat"));
-				a.setAltura(rs.getInt("altura"));
-				a.setOrigen(rs.getString("origen"));
-				
-				arboles.add(a);
-			}
-			
+			//Recorrer y guardar en el arrayList
+				while(rs.next()){
+					Arbol a = new Arbol();
+					a.setId(rs.getInt("id"));
+					a.setNombreComun(rs.getString("nombre_comun"));
+					a.setNombreCientifico(rs.getString("nombre_cientifico"));
+					a.setHabitat(rs.getString("habitat"));
+					a.setAltura(rs.getInt("altura"));
+					a.setOrigen(rs.getString("origen"));
+					
+					arboles.add(a);
+				}	
 		} catch (SQLException e) {
 			System.out.println("Error al cargar la tabla");
 			e.printStackTrace();
-		}
-		return arboles;
-		
+		}		
 	}
 	
-
+	private static void menu() {
+		System.out.println("--Menu--");
+		System.out.println(CREATE+"-Create");
+		System.out.println(READ+"-Read");
+		System.out.println(UPDATE+"-Update");
+		System.out.println(DELETE+"-Delete");
+		System.out.println(SALIR+"-Salir");
+	}
 }
