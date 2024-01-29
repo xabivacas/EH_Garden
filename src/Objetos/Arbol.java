b605ba7cad6999ab20eea734dd5bdb50a8fa38f2
Xabier Vacas <ikcty@plaiaundi.net>
2024-01-29 12:52:19 +0100

    Atributos arbol

--- src/Objetos/Arbol.java
+++ src/Objetos/Arbol.java
@@ -1,5 +1,53 @@
 package Objetos;
 
 public class Arbol {
-
+	
+	//Atributos
+		private int id;
+		private String nombreComun;
+		private String nombreCientifico;
+		private String habitat;
+		private int altura;
+		private String origen;
+	
+	//Getter&Setter
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getNombreComun() {
+			return nombreComun;
+		}
+		public void setNombreComun(String nombreComun) {
+			this.nombreComun = nombreComun;
+		}
+		public String getNombreCientifico() {
+			return nombreCientifico;
+		}
+		public void setNombreCientifico(String nombreCientifico) {
+			this.nombreCientifico = nombreCientifico;
+		}
+		public String getHabitat() {
+			return habitat;
+		}
+		public void setHabitat(String habitat) {
+			this.habitat = habitat;
+		}
+		public int getAltura() {
+			return altura;
+		}
+		public void setAltura(int altura) {
+			this.altura = altura;
+		}
+		public String getOrigen() {
+			return origen;
+		}
+		public void setOrigen(String origen) {
+			this.origen = origen;
+		}
+		
+	
+	
 }
